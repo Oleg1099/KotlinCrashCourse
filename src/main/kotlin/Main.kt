fab858e8f8c560af7d51b5f710399b4d4a11b16d
Ordo <ordolanus@gmail.com>
2025-04-19 23:25:54 +0300

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,114 @@
+import java.math.BigInteger
+
+// TODO: Вывод данных
+
+//fun main() {
+//    // print - выводит бе пробелов!
+//    print("Hello World!")
+//    print("How are you?") // >>> Hello World!How are you?
+//
+//    println()
+//
+//    println("Как твои дела?")  // >>>  Как твои дела?
+//    println("Ты где? Не бойся!")   // >>> Ты где? Не бойся!
+//}
+
+
+// TODO: Переменные
+// fun main() {
+//    // val - неизменяемая переменная, var - изменяемая переменная
+////    val hello = "hello"
+////    var a = 123
+//
+//    // Указываем тип переменной принудительно
+//    val a: Int = 123
+//    println(a)
+//    val text: String = "text"
+//    println(text)
+//    // Работа с большими числами
+//    val l = BigInteger("2423423423423423434342342348278927436234786000000000000000000000000000000000000000000000")
+//    println(l)
+//    val l2: Long = 9_223_372_036_854_775_807L // Максимальное число long
+//    println(l2)
+//
+//
+//    // Сложение (конкатенация строк)
+//    val name1: String = "Name_"
+//    val name2: String = "_namE"
+//    val name3 = name1 + name2
+//    println(name3)
+//
+//    val a = 120
+//    val b = 10
+//    val result = a + b
+//    println("Результат: ${a} + ${b} = ${result}")
+//}
+
+// TODO: Работа с BigInteger
+//fun main() {
+//    // Создаем два больших числа
+//    val a = BigInteger("123456789012345678901234567890")
+//    val b = BigInteger("987654321098765432109876543210")
+//
+//    // Сложение
+//    val sum = a.add(b)
+//    println("Сложение: $a + $b = $sum")
+//    println(sum)
+//
+//    // Вычитание
+//    val difference = b.subtract(a)
+//    println("Вычитание: $b - $a = $difference")
+//
+//    // Умножение
+//    val product = a.multiply(b)
+//    println("Умножение: $a * $b = $product")
+//
+//    // Деление (целочисленное)
+//    val quotient = b.divide(a)
+//    println("Деление: $b / $a = $quotient")
+//
+//    // Остаток от деления
+//    val remainder = b.remainder(a)
+//    println("Остаток от деления: $b % $a = $remainder")
+//
+//    // Возведение в степень (например, a^3)
+//    val power = a.pow(3)
+//    println("Возведение в степень: $a^3 = $power")
+//}
+
+// TODO: Работа с Long
+//fun main() {
+//    val a: Long = 1234567890123456789L
+//    val b: Long = 987654321098765432L
+//
+//    // Сложение
+//    val sum = a + b
+//    println("$a + $b = $sum")
+//    println(sum)
+//
+//    // Вычитание
+//    val difference = a - b
+//    println("$a - $b = $difference")
+//
+//    // Умножение
+//    val product = a * b
+//    println("$a * $b = $product")
+//
+//    // Деление (целочисленное)
+//    val quotient = a / b
+//    println("$a / $b = $quotient")
+//
+//    // Остаток от деления
+//    val remainder = a % b
+//    println("$a % $b = $remainder")
+//
+//    // Возведение в степень (используем `toDouble()`, т.к. в Kotlin нет встроенного оператора степени для Long)
+//    val exponent = 3
+//    val power = Math.pow(a.toDouble(), exponent.toDouble()).toLong()
+//    println("$a^$exponent ≈ $power") // Приблизительное значение из-за ограничений Double
+//}
+
+// TODO: Null-safety
+fun main() {
+    
+}
